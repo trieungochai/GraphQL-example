fd89dc61bc6434aa8d4f041f413ebca2c347574e
Hai Trieu Ngoc <trieungochai.dev@gmail.com>
2022-07-03 20:56:42 +0700

    Designing an E-Commerce Schema

--- server.js
+++ server.js
@@ -8,14 +8,63 @@ const PORT = process.env.PORT || 3000;
 
 const schema = buildSchema(`
   type Query {
-    description: String
-    price: Float
+    products: [Product]
+    orders: [Order]
+  }
+
+  type Product {
+    id: ID!,
+    description: String!
+    review: [Review]
+    price: Float!
+  }
+
+  type Review {
+    rating: Int!
+    comment: String
+  }
+
+  type Order {
+    date: String!
+    subtotal: Float!
+    item: [OrderItem]
+  }
+
+  type OrderItem {
+    product: Product!
+    quantity: Int!
   }
 `);
 
 const root = {
-  description: "Red Shoe",
-  price: 42.12,
+  products: [
+    {
+      id: "redshoe",
+      description: "Red Shoe",
+      price: 42.12,
+    },
+    {
+      id: "bluejean",
+      description: "Blue Jeans",
+      price: 55.55,
+    },
+  ],
+  orders: [
+    {
+      date: "2005-05-05",
+      subtotal: 90.22,
+      item: [
+        {
+          product: {
+            id: "redshoe",
+            description: "Old Red Shoe",
+            price: 45.11,
+          },
+          quantity: 2,
+        },
+      ],
+    },
+  ],
 };
 
 app.use(
